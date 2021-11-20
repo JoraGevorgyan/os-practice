14be7c9034e2b7b5633ea02987d1685835f31389
Jora Gevorgyan <jora@ispras.ru>
2021-11-20 23:49:38 +0400

    add some sample project

---
+++ build-tools/make-example/printer.cpp
@@ -0,0 +1,7 @@
+#include "printer.hpp"
+
+void print(const std::string& message)
+{
+    std::cout << "got a message from user!!" << std::endl;
+    std::cout << message << std::endl;
+}
---
+++ build-tools/make-example/printer.hpp
@@ -0,0 +1,9 @@
+#ifndef PRINTER_HPP_
+#define PRINTER_HPP_
+
+#include <iostream>
+#include <string>
+
+void print(const std::string& message);
+
+#endif // PRINTER_HPP_
