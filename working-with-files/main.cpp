ef708d1f6316b7d4bae945d6f7c66bd5969b9ba2
Jora Gevorgyan <jora@ispras.ru>
2021-11-25 13:12:24 +0400

    complete the implementation of 'list_content' function

--- working-with-files/main.cpp
+++ working-with-files/main.cpp
@@ -10,6 +10,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <dirent.h>
 
 #include <iostream>
 #include <string>
@@ -72,7 +73,33 @@ int list_content(int argc, char** argv)
 		std::cerr << "One path is required only!!" << std::endl;
 		return -1;
 	}
+	const char* path_to_dir = argv[1];
+	DIR* tmp_dir = opendir(path_to_dir);
+	if (tmp_dir == nullptr)
+	{
+		std::cerr << "error when opening directory" << std::endl;
+		return errno;
+	}
 
+	while (true)
+	{
+		const int old_err = errno;
+		dirent* path_info = readdir(tmp_dir);
+		if (path_info == nullptr)
+		{
+			if (errno != old_err)
+			{
+				std::cerr << "something wrong when reading a directory" << std::endl;
+				return errno;
+			}
+			break;
+		}
+		std::cout << path_info->d_name << " ";
+	}
+
+	std::cout << "\n\nThat's all we have, bye." << std::endl;
+	auto err_code = closedir(tmp_dir);
+	check(err_code, "error when trying to close the directory " + std::string(path_to_dir));
 	return 0;
 }
 
